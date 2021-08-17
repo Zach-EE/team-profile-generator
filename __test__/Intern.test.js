0e47610e83d996ea8079adf5c1c8ef0439e3c675
Zach Holloway <zachholloway.n@gmail.com>
2021-08-17 12:18:53 -0400

    added test for getSchool() to Intern.test

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,16 +1,16 @@
 const { TestWatcher } = require('jest');
 const Intern = require('../lib/Intern');
 test('Can set school  via constructor arg', () => {
-  const testValue = 'Pheonix Online';
+  const testValue = 'Phoenix Online';
   const e = new Intern('Frank the Tank','123','test@gmail.com', testValue);
   expect(e.school).toBe(testValue);
 });
 
-// test('Can get school via getGithub()', () => {
-//   const testValue = 'testUserName';
-//   const e = new Intern('Frank the tank', '123', 'test@gmail.com', testValue);
-//   expect(e.getGithub()).toBe(testValue);
-// });
+test('Can get school via getSchool()', () => {
+  const testValue = 'Phoenix Online';
+  const e = new Intern('Frank the tank', '123', 'test@gmail.com', testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
 
 // test('Can get "Intern" via getRole()', () => {
 //     const testValue = 'Intern';
