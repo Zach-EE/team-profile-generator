7ad991c0b990f15994da0927f47cd6c70b26c01e
Zach Holloway <zachholloway.n@gmail.com>
2021-08-17 12:25:38 -0400

    added test for seetting officeNumber via constructor arg to Manager.test

---
+++ __test__/Manager.test.js
@@ -0,0 +1,19 @@
+const { TestWatcher } = require('jest');
+const Manager = require('../lib/Manager');
+test('Can set office#  via constructor arg', () => {
+  const testValue = '555-867-5309';
+  const e = new Manager('Frank the Tank','123','test@gmail.com', testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
+// test('Can get school via getSchool()', () => {
+//   const testValue = 'Phoenix Online';
+//   const e = new Manager('Frank the tank', '123', 'test@gmail.com', testValue);
+//   expect(e.getSchool()).toBe(testValue);
+// });
+
+// test('Can get "Intern" via getRole()', () => {
+//     const testValue = 'Manager';
+//     const e = new Manager('Frank the tank', '123', 'test@gmail.com', 'Phoenix Online');
+//     expect(e.getRole()).toBe(testValue);
+//   });
